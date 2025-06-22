b5e2f6a1bfd484235ac4a65829607514e61deb6c
LucasTavaresA <tavares.lassuncao@gmail.com>
2025-06-21 22:40:03 -0300

    Xtreme! speedup with asyncio and eager page loading

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import asyncio
+import aiohttp
+import aiofiles
 from selenium import webdriver
 
-# should be 256 but that wasn't working
+# should be 256 but that isn't working
 max_filename_length = 200
 
-# very smart function, reporposed from https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/utils/_utils.py#L627
+# very smart function, repurposed from https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/utils/_utils.py#L627
 def sanitize_filename(s):
     """Sanitizes a string so it could be used as part of a filename."""
     if s == '':
@@ -38,6 +41,7 @@ def get_available_driver():
     try:
         options = webdriver.ChromeOptions()
         options.add_argument("--headless=new")
+        options.page_load_strategy = 'eager'
         driver = webdriver.Chrome(options=options)
         return driver
     except WebDriverException:
@@ -46,13 +50,16 @@ def get_available_driver():
     try:
         options = webdriver.FirefoxOptions()
         options.add_argument("--headless")
+        options.page_load_strategy = 'eager'
         driver = webdriver.Firefox(options=options)
         return driver
     except WebDriverException:
         pass
 
     try:
-        driver = webdriver.Safari()
+        options = webdriver.SafariOptions()
+        options.page_load_strategy = 'eager'
+        driver = webdriver.Safari(options=options)
         return driver
     except Exception: # nice "Exception" buddy...
         pass
@@ -60,6 +67,7 @@ def get_available_driver():
     try:
         options = webdriver.EdgeOptions()
         options.add_argument("--headless=new")
+        options.page_load_strategy = 'eager'
         driver = webdriver.Edge(options=options)
         return driver
     except WebDriverException:
@@ -68,26 +76,43 @@ def get_available_driver():
     print("No supported browser found.\nPlease install: Chrome, Firefox, Safari or Edge.")
     exit(1)
 
-def main():
+
+async def download(session, url, save_path):
+    try:
+        async with aiofiles.open(save_path, 'xb') as f:
+            async with session.get(url) as resp:
+                content = await resp.read()
+                await f.write(content)
+    except FileExistsError:
+        pass
+
+
+async def main():
     import os
     import sys
-    import requests
     import getpass
     from selenium_recaptcha_solver import RecaptchaSolver
     from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.wait import WebDriverWait
 
     if len(sys.argv) <= 1:
         print("Usage: main.py <download_dir>")
         exit(1)
 
     download_dir = sys.argv[1]
-    username = input("Username: ")
+    username = input("Username or Email: ")
     password = getpass.getpass("Password: ")
 
+    print("Opening browser...")
     driver = get_available_driver()
     captcha_solver = RecaptchaSolver(driver=driver)
 
+    print("Logging in...")
     driver.get("https://nhentai.net/login/?next=/favorites/")
+
+    # recaptcha is too slow for 'eager' page load strategy
+    wait = WebDriverWait(driver, timeout=5)
+    wait.until(lambda _: driver.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]'))
 
     username_field = driver.find_element(by=By.NAME, value="username_or_email")
     password_field = driver.find_element(by=By.NAME, value="password")
@@ -102,31 +127,39 @@ def main():
     submit_button.click()
 
     selenium_cookies = driver.get_cookies()
-    session = requests.Session()
+    cookies = {cookie['name']: cookie['value'] for cookie in selenium_cookies}
 
-    for cookie in selenium_cookies:
-        session.cookies.set(cookie['name'], cookie['value'])
+    ids = []
+    titles = []
 
     while True:
-        gallery = driver.find_element(By.ID, "favcontainer")
-        favorites = gallery.find_elements(By.CLASS_NAME, "gallery-favorite")
+        favorites = driver.find_elements(By.CLASS_NAME, "gallery-favorite")
 
         for favorite in favorites:
-            id = favorite.get_attribute("data-id")
-            title = favorite.find_element(By.CLASS_NAME, "caption").text
-            save_path = os.path.join(os.path.expanduser(download_dir), sanitize_filename(title[:max_filename_length:]) + ".torrent")
-
-            try:
-                with open(save_path, "xb") as f:
-                    response = session.get(f"https://nhentai.net/g/{id}/download")
-                    f.write(response.content)
-            except FileExistsError:
-                pass
+            ids.append(favorite.get_attribute("data-id"))
+            titles.append(favorite.find_element(By.CLASS_NAME, "caption").text)
+            sys.stdout.write(f"\rLoading favorites {len(ids)}")
+            sys.stdout.flush()
 
         next_buttons = driver.find_elements(By.CLASS_NAME, "next")
         if next_buttons == []:
             break
 
         next_buttons[0].click()
+    print()
 
-main()
+    favorites_amount = len(ids)
+    print(f"Downloading {favorites_amount} favorites...")
+
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        tasks = []
+        for i in range(favorites_amount):
+            sys.stdout.write(f"\rDownloading favorite {i}")
+            sys.stdout.flush()
+            save_path = os.path.join(os.path.expanduser(download_dir), sanitize_filename(titles[i][:max_filename_length:]) + ".torrent")
+            task = asyncio.create_task(download(session, f"https://nhentai.net/g/{id}/download", save_path))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+    print()
+
+asyncio.run(main())
